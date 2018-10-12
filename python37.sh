c0472052ecc756f7b55fd8ee9b20a1a4bbeceac6
cnrsf80 <43200578+cnrsf80@users.noreply.github.com>
2018-10-12 10:43:22 +0200

    Create python37.sh

---
+++ python37.sh
@@ -0,0 +1,26 @@
+#Installation de Python37 sur raspbian
+
+sudo apt-get update -y
+sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev -y
+sudo apt-get install libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+
+#Build
+wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
+tar xf Python-3.7.0.tar.xz
+cd Python-3.7.0
+./configure
+make -j 4
+sudo make altinstall
+
+#Cleanning
+sudo rm -r Python-3.7.0
+rm Python-3.7.0.tar.xz
+sudo apt-get --purge remove build-essential tk-dev -y
+sudo apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev -y
+sudo apt-get --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev -y
+sudo apt-get --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+sudo apt-get autoremove -y
+sudo apt-get clean
+
+#Connexion
+python3
