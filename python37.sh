f4c8c81fbefb494752f3e891b651fdc871b6986a
cnrsf80 <43200578+cnrsf80@users.noreply.github.com>
2018-10-12 16:42:51 +0200

    Update python37.sh

--- python37.sh
+++ python37.sh
@@ -4,9 +4,9 @@ apt-get remove -y python3
 apt-get remove -y 'python3.*'
 
 #Installation de Python37 sur raspbian
-sudo apt-get update -y
-sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev -y
-sudo apt-get install libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+apt-get update -y
+apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev
+apt-get install -y libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 
 #Build
 wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tar.xz
@@ -14,7 +14,7 @@ tar xf Python-3.7.0.tar.xz
 cd Python-3.7.0
 ./configure
 make -j 4
-sudo make altinstall
+make altinstall
 
 #Tools pip
 apt-get install python-pip3.7
@@ -34,12 +34,12 @@ pip install scikit-learn
 #Cleanning
 sudo rm -r Python-3.7.0
 rm Python-3.7.0.tar.xz
-sudo apt-get --purge remove build-essential tk-dev -y
-sudo apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev -y
-sudo apt-get --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev -y
-sudo apt-get --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
-sudo apt-get autoremove -y
-sudo apt-get clean
+apt-get --purge remove build-essential tk-dev -y
+apt-get --purge remove libncurses5-dev libncursesw5-dev libreadline6-dev -y
+apt-get --purge remove libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev -y
+apt-get --purge remove libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
+apt-get autoremove -y
+apt-get clean
 
 
 
