112e1f1e2378277da0ea8ec47d7d801802da20a6
cnrsf80 <43200578+cnrsf80@users.noreply.github.com>
2018-10-13 10:33:03 +0200

    Update python37.sh

--- python37.sh
+++ python37.sh
@@ -19,6 +19,12 @@ make altinstall
 #Tools pip
 apt-get install python-pip3.7
 
+#Prepare raspberry for scipy building https://raspberrypi.stackexchange.com/questions/8308/how-to-install-latest-scipy-version-on-raspberry-pi
+sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+sudo /sbin/mkswap /var/swap.1
+sudo chmod 600 /var/swap.1
+sudo /sbin/swapon /var/swap.1
+
 #Alias
 alias python=python3.7
 alias python3=python3.7
